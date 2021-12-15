f54f6a00d0c5f84acc5fd1f18ef53a4b0645f27d
Ravneet Panglia <ravneet.panglia@gmail.com>
2021-12-15 00:21:31 -0500

    tests added

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,11 @@
+// const { test } = require('picomatch');
+// const { expect } = require('@jest/globals');
 const Employee = require('../lib/Employee');
 
 test('create an employee object', () => {
-    const employee = new Employee('Steve');
+    const employee = new Employee('Steve', 149, 'stevee@gmail.com');
 
-    expect(employee.name).toBe('Steve');
-    expect(employee.id).toBe(expect.any(Number));
-    expect(employee.email).toBe(expect.any(String));
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
 });
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,12 @@
+// const { test } = require('picomatch');
+// const { expect } = require('@jest/globals');
 const Manager = require('../lib/Manager');
+
+test('create manager object', () => {
+    const manager = new Manager('Steve', 149, 'stevee@gmail.com', 14);
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+});

8caa1386bfdabcf93e51122e32b8c8da67bf02b9
Ravneet Panglia <ravneet.panglia@gmail.com>
2021-12-14 23:50:16 -0500

    added dist and src folders, worked on index.js

--- index.js
+++ index.js
@@ -7,6 +7,40 @@ const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
 
+const inputManager = addManagerInfo => {
+    inquirer.prompt([ 
+        {
+            type: 'input',
+            message: "What is your Manager's name?",
+            name: 'name'
+        },
+        {
+            type: 'input',
+            message: "What is your Manager's ID?",
+            name: 'id'
+        },
+        {
+            type: 'input',
+            message: "What is your Manager's email address?",
+            name: 'email'
+        },
+        {
+            type: 'input',
+            message: "What is your Manager's office number?",
+            name: 'officeNumber'
+        }
+    ])
+    .then(addManagerInfo => {
+        const { name, id, email, officeNumber } = addManagerInfo;
+        const manager = new Manager (name, id, email, officeNumber);
+        console.log(manager);
+    })
+
+}
+
+//Function call
+inputManager();
+
 
 // inquirer
 //     .prompt(
@@ -19,6 +53,10 @@ const Manager = require('./lib/Manager');
 //             name: 'name'
 //         },
 //     ])
+// .then((response) => {
+//     console.log(response);
+// })
+
 
 
 //FIRST: prompt for team managers name, id, email and office number (make function for manager profile)

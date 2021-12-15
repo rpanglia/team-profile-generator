eddabae3d9ab476a2b2730af8791df7b009794d1
Ravneet Panglia <ravneet.panglia@gmail.com>
2021-12-14 19:27:53 -0500

    updating index, test added to employee and constructor added to lib/employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,10 @@
+const { expect } = require('@jest/globals');
 const Employee = require('../lib/Employee');
+
+test('create an employee object', () => {
+    const employee = new Employee('Steve');
+
+    expect(employee.name).toBe('Steve');
+    expect(employee.id).toBe(expect.any(Number));
+    expect(employee.email).toBe(expect.any(String));
+});
--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+
+//For Employee: name, id, email, getName(), getId(), getEmail(), getRole()
+//For Engineer: name, id, email, getName(), getId(), getEmail(), github [username], getGitHub(), getRole()
+//For Intern: name, id, email, getName(), getId(), getEmail(), school, getSchool(), getRole()
+//For Manager: name, id, email, getName(), getId(), getEmail(), officeNumber, getRole()
+
+// inquirer
+//     .prompt(
+//         questions
+//     )
+//     .prompt([
+//         {
+//             type: 'input',
+//             message: "Employee name?",
+//             name: 'name'
+//         },
+//     ])
+    //ex of what it will look like. add id, email, etc

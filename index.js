b51b8ad167bc650a5e81655da141201084e2d995
Ravneet Panglia <ravneet.panglia@gmail.com>
2021-12-15 21:41:48 -0500

    questions for adding new employee

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const allEmployees = [];
 const { createManagerCard, createInternCard, createEngineerCard } = require('./src/template-html');
 const generateHTML = require('./src/create-html');
 
+
 //add validation if possible
 const inputManager = addManagerInfo => {
     return inquirer.prompt([ 
@@ -37,11 +38,54 @@ const inputManager = addManagerInfo => {
     .then(addManagerInfo => {
         const { name, id, email, officeNumber } = addManagerInfo;
         const manager = new Manager (name, id, email, officeNumber);
+
+        var addManager = createManagerCard(manager);
+        allEmployees.push(addManager);
         console.log(manager);
 
     })
-
 }
+
+const addEmployee = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            message: "What is the role of this new employee?",
+            name: 'role',
+            choices: ['Intern', 'Engineer']
+        },
+        {
+            type: 'input',
+            message: "What is the name of this new employee?",
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: "What is the ID of this new employee?",
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: "What is the email address of this new employee?",
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: "What school is this Intern from?",
+            name: 'school',
+            when: (input) => input.role === 'Intern'
+        },
+        {
+            type: 'input',
+            message: "What GitHub username belongs to this Engineer?",
+            name: 'github',
+            when: (input) => input.role === 'Engineer'
+        }
+    ])
+}
+
+
+
 
 //Function call
 inputManager();
--- src/create-html.js
+++ src/create-html.js
@@ -1,9 +1,26 @@
-const Employee = require('../lib/Employee');
-const Engineer = require('../lib/Engineer');
-const Intern = require('../lib/Intern');
-const Manager = require('../lib/Manager');
+const fs = require('fs');
 
+// const Employee = require('../lib/Employee');
+// const Engineer = require('../lib/Engineer');
+// const Intern = require('../lib/Intern');
+// const Manager = require('../lib/Manager');
 
-function createManagerCard(manager) {
-    
+const generateHTML = data => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+        <title>Document</title>
+    </head>
+    <body>
+        ${data}
+    </body>
+    </html>
+    `
 }
+
+module.exports = generateHTML;

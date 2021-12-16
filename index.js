ba306f0c1537db1316aee1d4a7edded7afe36f55
Ravneet Panglia <ravneet.panglia@gmail.com>
2021-12-15 22:44:09 -0500

    updated index.js and added index.html

--- index.js
+++ index.js
@@ -80,35 +80,47 @@ const addEmployee = () => {
             message: "What GitHub username belongs to this Engineer?",
             name: 'github',
             when: (input) => input.role === 'Engineer'
+        },
+        {
+            type: 'confirm',
+            message: "Would you like to add another employee?",
+            name: 'confirmNewEmployee',
+            default: false
         }
     ])
+    .then (newEmployee => {
+        let { name, id, role, email, github, school, confirmNewEmployee} = newEmployee;
+        if (role === 'Intern') {
+            let employee = new Intern (name, id, email, school);
+            var addIntern = createInternCard(employee);
+            allEmployees.push(addIntern);
+            console.log(employee);
+        }
+        else if (role === 'Engineer') {
+            let employee = new Engineer (name, id, email, github);
+            var addEngineer = createEngineerCard(employee);
+            allEmployees.push(addEngineer);
+            console.log(employee);
+        }
+        if (confirmNewEmployee) {
+            return addEmployee();
+        }
+        else {
+            console.log(allEmployees);
+            var html = generateHTML(allEmployees);
+            fs.writeFile('index.html', html, function(err) {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log('Success! You have added a new Employee.');
+            })
+        }
+    })
 }
 
-
-
-
 //Function call
-inputManager();
-
-// init();
-
-
-// inquirer
-//     .prompt(
-//         questions
-//     )
-//     .prompt([
-//         {
-//             type: 'input',
-//             message: "Employee name?",
-//             name: 'name'
-//         },
-//     ])
-// .then((response) => {
-//     console.log(response);
-// })
-
-
+inputManager()
+.then(addEmployee);
 
 //FIRST: prompt for team managers name, id, email and office number (make function for manager profile)
 //THEN option to add engineer or intern or finish builiding team

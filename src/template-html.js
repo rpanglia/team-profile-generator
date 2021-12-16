2d3b63b08a52506fbe3327807c99b1d85c83a0d0
Ravneet Panglia <ravneet.panglia@gmail.com>
2021-12-16 00:33:53 -0500

    cards generated with correct data, links working

--- src/template-html.js
+++ src/template-html.js
@@ -8,7 +8,7 @@ const createManagerCard = data => {
         <div class="card-body">
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">ID: ${data.id}</li>
-                <li class="list-group-item">Email: <a href="mailto:${data.email()}">${data.email}</a></li>
+                <li class="list-group-item">Email: <a href="mailto:${data.email}">${data.email}</a></li>
                 <li class="list-group-item">Office Number: ${data.officeNumber}</li>
             </ul>
         </div>
@@ -26,7 +26,7 @@ const createInternCard = data => {
         <div class="card-body">
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">ID: ${data.id}</li>
-                <li class="list-group-item">Email: <a href="mailto:${data.email()}">${data.email}</a></li>
+                <li class="list-group-item">Email: <a href="mailto:${data.email}">${data.email}</a></li>
                 <li class="list-group-item">School: ${data.school}</li>
             </ul>
         </div>
@@ -44,8 +44,8 @@ const createEngineerCard = data => {
         <div class="card-body">
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">ID: ${data.id}</li>
-                <li class="list-group-item">Email: <a href="mailto:${data.email()}">${data.email}</a></li>
-                <li class="list-group-item">GitHub: <a href="https://github.com/rpanglia" target="_blank">${data.github}</a></li>
+                <li class="list-group-item">Email: <a href="mailto:${data.email}">${data.email}</a></li>
+                <li class="list-group-item">GitHub: <a href="https://github.com/${data.github}" target="_blank">${data.github}</a></li>
             </ul>
         </div>
     </div>
